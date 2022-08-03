1602df718becffc354f699caa56b5d15b78296d7
Серков Евгений <exzojs@gmail.com>
2022-08-03 14:23:01 +0300

    second

---
+++ src/test/java/ru/netology/statistic/jacocomvn/StatisticsServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.statistic.jacocomvn;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithOneMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {3, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
+
